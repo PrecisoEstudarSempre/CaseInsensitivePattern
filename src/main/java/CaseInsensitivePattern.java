9f8f1c0716f886d1068c433c9310500d0668f60e
Preciso Estudar Sempre <precisoestudarsempre@gmail.com>
2016-11-06 13:50:13 -0200

    Modificações para a postagem

--- src/main/java/CaseInsensitivePattern.java
+++ src/main/java/CaseInsensitivePattern.java
@@ -2,10 +2,12 @@ import java.util.regex.Pattern;
 
 class CaseInsensitivePattern {
 	public static void main(String[] args) {
-		System.out.println("Case insensitive regex: " + Pattern.compile("select",Pattern.CASE_INSENSITIVE).matcher("SELECT").matches());
-		System.out.println("Case sensitive regex: " + Pattern.compile("select").matcher("SELECT").matches());
+		//Primeira forma de aplicação através da constante provida pela própria API.
+		System.out.println("Case insensitive match via constant: " + Pattern.compile("^([a-z]*\\s*)*$",Pattern.CASE_INSENSITIVE).matcher("PrEcIsO eStUdAr SeMpRe").matches());
+		System.out.println("Case sensitive match via constant: " + Pattern.compile("^([a-z]*\\s*)*$").matcher("PrEcIsO eStUdAr SeMpRe").matches());
 
-		System.out.println("Case insensitive match through String: " + "SELECT".matches("(?i)select"));
-		System.out.println("Case sensitive match through String: " + "SELECT".matches("select"));
+		//Segunda forma de aplicação através de uma flag na própria regex.
+		System.out.println("Case insensitive match via String: " + "PrEcIsO eStUdAr SeMpRe".matches("(?i)^([a-z]*\\s*)*$"));
+		System.out.println("Case sensitive match via String: " + "PrEcIsO eStUdAr SeMpRe".matches("^([a-z]*\\s*)*$"));
 	}
 }
